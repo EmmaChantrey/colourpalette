883c13aa78924fc5cd6e4646a40e1349bf2f346d
EmmaChantrey <87299297+EmmaChantrey@users.noreply.github.com>
2023-09-04 21:16:43 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+const refreshBtn = document.querySelector(".button");
+const colours = document.querySelector(".colours");
+const paletteBoxes = 5;
+const lockedColors = new Set();
+let initialPalette = [];
+
+const generatePalette = () => {
+  colours.innerHTML = "";
+  
+  const newPalette = [];
+
+  // Generate new colors for the unlocked slots
+  for (let i = 0; i < paletteBoxes; i++) {
+    let randomHex;
+    
+    if (initialPalette[i] !== undefined && lockedColors.has(initialPalette[i])) {
+      // If a color is locked in the initial palette, keep it
+      randomHex = initialPalette[i];
+    } else {
+      do {
+        randomHex = generateRandomColor();
+      } while (newPalette.includes(randomHex) || lockedColors.has(randomHex));
+    }
+
+    newPalette.push(randomHex);
+
+    const colour = document.createElement("li");
+    colour.classList.add("colour");
+    colour.innerHTML = `<div class="colourimage" style="background: ${randomHex}"></div>
+                         <div class="hex">${randomHex}</div>`;
+    colour.addEventListener("click", () => toggleLockColour(colour, randomHex));
+    colours.appendChild(colour);
+  }
+
+  initialPalette = [...newPalette];
+};
+
+const generateRandomColor = () => {
+  return `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, "0")}`;
+};
+
+const toggleLockColour = (colourElement, colorHex) => {
+  if (lockedColors.has(colorHex)) {
+    lockedColors.delete(colorHex);
+    colourElement.classList.remove("locked");
+  } else {
+    lockedColors.add(colorHex);
+    colourElement.classList.add("locked");
+  }
+};
+
+generatePalette();
+
+refreshBtn.addEventListener("click", generatePalette);
